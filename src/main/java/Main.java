2f04d1964e5e9327d65de71a1f4d95b865ba7513
AgustinSego <agustin.segovia.p@mail.pucv.cl>
2026-08-26 12:09:10 -0400

    leer csv alpha

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,23 @@
+import java.util.*;
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        //ArrayList<PuestoDeTrabajo> lista = new ArrayList<>();
+        //path del csv
+        String archivo ="C:\\Users\\Lenovo\\IdeaProjects\\Manejo-de-Bolsa-de-Trabajo\\src\\Hoja de cálculo sin título - Hoja 1.csv";
+        //
+        String linea = "";
+        //
+        String separador = ",";
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo))){
+            while((linea = br.readLine()) != null){
+                //cambiar para el hasp y arraylist
+                String []datos = linea.split(separador);
+                System.out.println(datos[0] + "-" + datos[1] + "-" + datos[2] + "-" + datos[3]);
+            }
+        }
+        catch (IOException e){e.printStackTrace();}
+
     }
 }
