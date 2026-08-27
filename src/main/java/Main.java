b7b6c8157737d1d792b7ab6349a0473c5ad525d8
AgustinSego <agustin.segovia.p@mail.pucv.cl>
2026-08-26 20:36:27 -0400

    leer csv alpha 0.0.4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,34 +2,33 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        ArrayList<String> keyPuestoTrabajo = new ArrayList<>();
-        HashMap<String,ArrayList<PuestoDeTrabajo>> mapaPuestoTrabajo = new HashMap<>();
+    public static void main(String[] args){
+        ArrayList<String> keysTrabajo = new ArrayList<>();
+        HashMap<String, ArrayList<PuestoDeTrabajo>> mapaPuestoTrabajo = new HashMap<>();
 
-        //PROCESAMIENTO CSV
-        //path del csv
-        String archivo ="src/Hoja de cálculo sin título - Hoja 1.csv";
+        leerCsv(mapaPuestoTrabajo, keysTrabajo, "src/Puestos de trabajo.csv");
+        mostrar(mapaPuestoTrabajo, keysTrabajo);
+    }
+    public static void leerCsv(HashMap<String, ArrayList<PuestoDeTrabajo>> mapa, ArrayList<String> keys, String path){
         String linea;
-        String separador = ",";
-        try (BufferedReader br = new BufferedReader(new FileReader(archivo))){
+        String separador =",";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))){
             while((linea = br.readLine()) != null){
-                //cambiar para el hasp y arraylist
                 String []datos = linea.split(separador);
-                if(mapaPuestoTrabajo.containsKey(datos[1])){
-                    ArrayList listaMap = mapaPuestoTrabajo.get(datos[1]);
+                if(mapa.containsKey(datos[1])){
+                    ArrayList listaMap = mapa.get(datos[1]);
                     listaMap.add(new PuestoDeTrabajo(datos[0], datos[1], datos[2], Integer.parseInt(datos[3].trim()), Integer.parseInt(datos[4].trim())));
                 }else{
                     ArrayList<PuestoDeTrabajo> lista = new ArrayList<>();
                     lista.add(new PuestoDeTrabajo(datos[0], datos[1], datos[2], Integer.parseInt(datos[3].trim()), Integer.parseInt(datos[4].trim())));
-                    mapaPuestoTrabajo.put(datos[1], lista);
-                    keyPuestoTrabajo.add(datos[1]);
+                    mapa.put(datos[1], lista);
+                    keys.add(datos[1]);
                 }
             }
-        }
-        catch (IOException e){e.printStackTrace();}
-        mostrar(mapaPuestoTrabajo , keyPuestoTrabajo);
-
+        }catch (IOException e){e.printStackTrace();}
     }
+
     public static void mostrar(HashMap<String, ArrayList<PuestoDeTrabajo>> map, ArrayList<String> keys){
         for(String clave: keys){
             ArrayList<PuestoDeTrabajo> lista = map.get(clave);
